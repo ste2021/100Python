a75065db665b35e4a91656852353dd4924558475
ste2021 <estela.oliveira.cont@gmail.com>
2022-08-16 10:33:48 -0300

    day1-Exercise1-2

---
+++ day1/day1-exercise2.py
@@ -0,0 +1,23 @@
+# Fix the code below
+st_mais = "\"+\""
+st_hello = "\"Hello \""
+st_world = "\"world\""
+
+# 1 - Missing double quotes before the word Day.
+# print(Day 1 - String Manipulation")
+print("Day 1 - String Manipulation")
+
+# 2 - Outer double quotes changed to single quotes.
+# print(f'String Concatenation is done with the {st_mais} sign.')
+print(f'String Concatenation is done with the {st_mais} sign.')
+
+# 3 - Extra indentation removed
+# print(f'e.g. print({st_hello} + {st_world})')
+print(f'e.g. print({st_hello} + {st_world})')
+
+# 4 - Extra (in print function removed)
+# print("New lines can be created with a backslash and n.")
+print("New lines can be created with a backslash and n.")
+
+
+
