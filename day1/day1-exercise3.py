6fc61b003fe2696cddbaf006bd878d280eee4857
ste2021 <estela.oliveira.cont@gmail.com>
2022-08-17 09:08:45 -0300

    Add files Exercises 2-3

---
+++ day1/day1-exercise3.py
@@ -0,0 +1,9 @@
+# Your program should work for different inputs. e.g. any name that you input.
+
+def input_function():
+
+    input1 = input("Qual o seu nome?")
+    if input1 == 'Angela':
+        print(6)
+
+input_function()
